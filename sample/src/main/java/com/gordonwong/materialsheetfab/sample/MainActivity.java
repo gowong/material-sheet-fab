89505edfadcd28b84a7ee343ddb346a8ca8588ea
Gordon Wong <gwong001@gmail.com>
2015-07-18 00:14:39 -0700

    Add navigation drawer

--- sample/src/main/java/com/gordonwong/materialsheetfab/sample/MainActivity.java
+++ sample/src/main/java/com/gordonwong/materialsheetfab/sample/MainActivity.java
@@ -3,11 +3,15 @@ package com.gordonwong.materialsheetfab.sample;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -21,6 +25,8 @@ import com.gordonwong.materialsheetfab.MaterialSheetFabEventListener;
  */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+	private ActionBarDrawerToggle drawerToggle;
+	private DrawerLayout drawerLayout;
 	private MaterialSheetFab materialSheetFab;
 	private int statusBarColor;
 
@@ -29,9 +35,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		super.onCreate(savedInstanceState);
 		setTitle(R.string.notes);
 		setContentView(R.layout.activity_main);
-		setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+		setupActionBar();
+		setupDrawer();
 		setupTabs();
 		setupFab();
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		drawerToggle.syncState();
 	}
 
 	@Override
@@ -41,6 +54,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		} else {
 			super.onBackPressed();
 		}
+	}
+
+	/**
+	 * Sets up the action bar.
+	 */
+	private void setupActionBar() {
+		setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+
+	/**
+	 * Sets up the navigation drawer.
+	 */
+	private void setupDrawer() {
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.opendrawer,
+				R.string.closedrawer);
+		drawerLayout.setDrawerListener(drawerToggle);
 	}
 
 	/**
@@ -99,6 +130,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		findViewById(R.id.fab_sheet_item_note).setOnClickListener(this);
 	}
 
+	/**
+	 * Toggles opening/closing the drawer.
+	 */
+	private void toggleDrawer() {
+		if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+			drawerLayout.closeDrawer(GravityCompat.START);
+		} else {
+			drawerLayout.openDrawer(GravityCompat.START);
+		}
+	}
+
 	@Override
 	public void onClick(View v) {
 		Toast.makeText(this, R.string.sheet_item_pressed, Toast.LENGTH_SHORT).show();
@@ -110,6 +152,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.menu_main, menu);
 		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			toggleDrawer();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 	private int getStatusBarColor() {
